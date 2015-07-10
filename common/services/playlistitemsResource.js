abee1ac0f6400b0b27930aae825013476e817112
A.Ovsiannikov <a.ovsiannikov@levi9.com>
2015-07-10 14:35:06 +0300

    add search and playlistitems resources

---
+++ common/services/playlistitemsResource.js
@@ -0,0 +1,27 @@
+/**
+ * Created by a.ovsiannikov on 7/10/2015.
+ */
+(function(){
+    "use strict";
+
+    angular
+        .module("common.services")
+        .factory("playlistitemsResource", ["$resource",playlistitemsResource]);
+
+    function playlistitemsResource($resource) {
+        return $resource(
+            "https://www.googleapis.com/youtube/v3/playlistItems",null
+            ,
+            {
+                'get':    {method:'GET',params:{id:'@id'}},
+                'save':   {method:'POST'},
+                'query':  {method:'GET', params:{part: 'snippet', playlistId: '@playlistId', mine: 'true', maxResults : 50}, withCredentials:true},
+                'remove': {method:'DELETE'},
+                'delete': {method:'DELETE'}
+            }
+        );
+    }
+}());
+
+
+
