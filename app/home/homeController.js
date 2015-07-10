8a543fc119523339262b074ce31d5c655bc2bcf0
d.berzoy <d.berzoy@gmail.com>
2015-07-10 18:23:07 +0300

    Added part of EditView layout and EditMode implementation

--- app/home/homeController.js
+++ app/home/homeController.js
@@ -6,27 +6,48 @@
 
     angular
         .module("ytApp")
-        .controller("HomeController", ["playlistResource", HomeController]);
+        .controller("HomeController", ["playlistResource", "$state", HomeController]);
 
-    function HomeController(playlistResource) {
+    function HomeController(playlistResource, $state) {
         var vm = this;
         vm.playlists = [];
         vm.videos = [];
         vm.selectedPlayList = {};
-        vm.videos = [
-            {name: "pupies"}, {name: "kitties"}, {name: "parrots"}, {name: "kitties"}, {name: "parrots"}, {name: "kitties"}, {name: "parrots"}, {name: "kitties"}, {name: "parrots"}, {name: "kitties"}, {name: "parrots"}, {name: "kitties"}, {name: "parrots"}, {name: "kitties"}, {name: "parrots"}, {name: "kitties"}, {name: "parrots"}, {name: "kitties"}, {name: "parrots"}, {name: "kitties"}, {name: "parrots"}, {name: "kitties"}, {name: "parrots"}, {name: "kitties"}, {name: "parrots"}, {name: "kitties"}, {name: "parrots"}, {name: "kitties"}, {name: "parrots"}, {name: "kitties"}, {name: "parrots"}
-        ];
 
         vm.changePlayList = function(playlist){
             if(!vm.selectedPlayList || vm.selectedPlayList.id !== playlist.id){
                 vm.selectedPlayList = playlist;
             }
-        }
+        };
+
+        vm.currentView = function(){
+            return $state.current.name;
+        };
 
         playlistResource.query(function(data){
             for(var i = 0; i < data.items.length; i++){
                 vm.playlists.push(data.items[i]);
             }
         });
+
+        vm.toggleEdit = function(){
+            if(isEditMode()){
+                $state.go(prevView == 'home.edit' ? 'home.player' : prevView);
+            }
+            else{
+                prevView = vm.currentView();
+                $state.go('home.edit');
+            }
+        };
+
+        var prevView;
+
+        var isEditMode = function(){
+            return vm.currentView == 'home.edit';
+        };
+
+        vm.removePlaylist = function(playlist){
+
+        };
     }
 }());
