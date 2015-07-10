0003a4431583e829f5b127a572cd4da972592e59
A.Ovsiannikov <a.ovsiannikov@levi9.com>
2015-07-10 16:11:48 +0300

    example how to use 2 resources

--- app/home/homeController.js
+++ app/home/homeController.js
@@ -6,13 +6,30 @@
 
     angular
         .module("ytApp")
-        .controller("HomeController", HomeController);
+        .controller("HomeController",["playlistResource","playlistitemsResource", HomeController]);
 
-    function HomeController() {
+    function HomeController(playlistResource,playlistitemsResource) {
         var vm = this;
         vm.videos = [
             {name: "pupies"}, {name: "kitties"}, {name: "parrots"}, {name: "kitties"}, {name: "parrots"}, {name: "kitties"}, {name: "parrots"}, {name: "kitties"}, {name: "parrots"}, {name: "kitties"}, {name: "parrots"}, {name: "kitties"}, {name: "parrots"}, {name: "kitties"}, {name: "parrots"}, {name: "kitties"}, {name: "parrots"}, {name: "kitties"}, {name: "parrots"}, {name: "kitties"}, {name: "parrots"}, {name: "kitties"}, {name: "parrots"}, {name: "kitties"}, {name: "parrots"}, {name: "kitties"}, {name: "parrots"}, {name: "kitties"}, {name: "parrots"}, {name: "kitties"}, {name: "parrots"}
         ];
+
+/*
+        var playerList = playlistResource.query(
+            function()
+            {
+                console.log(playerList.items);
+                var items = playerList.items;
+                for(var item = 0; item < items.length; item++)
+                {
+                    var lists = playlistitemsResource.query({playlistId: items[item].id},function()
+                    {
+                        console.log(lists);
+                    });
+                }
+            }
+        );
+*/
 
         vm.playlists = [
             {name: "Animals"}, {name: "Cars"}, {name: "Fails"}
