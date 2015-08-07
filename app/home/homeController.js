21ecd2f15759f6f6ff37f5d0dc70394d7f958b03
kristina <kristina.lyba@gmail.com>
2015-08-07 15:28:49 +0300

    few issues fixed

--- app/home/homeController.js
+++ app/home/homeController.js
@@ -1,39 +1,46 @@
 (function () {
-    "use strict";
+    'use strict';
 
     angular
-        .module("ytApp")
-        .controller("HomeController", ["$scope", "playlistService", "$state", HomeController]);
+        .module('ytApp')
+        .controller('HomeController', ['$scope', 'playlistService', '$state', HomeController]);
 
     function HomeController($scope, playlistService, $state) {
         var vm = this;
         vm.playlistService = playlistService;
         vm.selectedPlaylist = {};
+        vm.selectedPlaylist.empty = true;
         vm.selectedPlaylistItem = {};
         vm.isVideoInCurrentPlaylist = true;
-        vm.searchText='';
+        vm.searchText = '';
 
-        vm.getSearchResult = function(){
-            if(vm.searchText !== ''){
-                $state.go('home.search', {searchText: vm.searchText});
+        vm.getSearchResult = function () {
+            if (vm.searchText !== '') {
+                $state.go('home.search', {
+                    searchText: vm.searchText
+                });
             }
         };
 
-        playlistService.playlistsPromise.then(function(){
+        playlistService.playlistsPromise.then(function () {
             if (vm.playlistService.playlists.length) {
                 vm.setCurrentPlaylist(vm.playlistService.playlists[0]);
             }
         });
 
         vm.addToPlaylist = function () {
-            var newItem = {id: vm.selectedPlaylistItem};
+            var newItem = {
+                id: vm.selectedPlaylistItem
+            };
 
             playlistService.addItemToPlaylist(vm.selectedPlaylist, newItem)
                 .then(checkIsVideoInCurrentPlaylist);
         };
 
         vm.removeFromPlaylist = function (item) {
-            var itemToDelete = {id: item? item.id : vm.selectedPlaylistItem.id};
+            var itemToDelete = {
+                id: item ? item.id : vm.selectedPlaylistItem.id
+            };
 
             playlistService.removeItemFromPlaylist(vm.selectedPlaylist, itemToDelete)
                 .then(checkIsVideoInCurrentPlaylist);
@@ -41,10 +48,10 @@
 
         var checkIsVideoInCurrentPlaylist = function () {
             var result = true;
-            if(vm.selectedPlaylist){
+            if (vm.selectedPlaylist) {
                 result = _.findIndex(vm.selectedPlaylist.items, function (item) {
                     return item.snippet.resourceId.videoId === vm.selectedPlaylistItem.videoId;
-                }) !== -1 ;
+                }) !== -1;
             }
             vm.isVideoInCurrentPlaylist = result;
         };
@@ -53,51 +60,50 @@
             if (!vm.selectedPlaylist || vm.selectedPlaylist.id !== playlist.id) {
                 $scope.selectedPlaylist = playlist;
                 vm.selectedPlaylist = playlist;
-                if(!playlist.items.length){
+                if (!playlist.items.length) {
                     playlistService.fillPlaylistItems(playlist)
                         .then(function () {
-                            if(playlist.items.length)
+                            if (playlist.items.length) {
                                 vm.setCurrentPlaylistItem(playlist.items[0]);
-                        }
-                    );
+                                vm.selectedPlaylist.empty = false;
+                            } else {
+                                vm.selectedPlaylist.empty = true;
+                            }
+                        });
                 } else {
                     vm.setCurrentPlaylistItem(playlist.items[0]);
                 }
             }
         };
 
-        vm.setCurrentPlaylistItem = function(playlistItem){
-            if(playlistItem){
+        vm.setCurrentPlaylistItem = function (playlistItem) {
+            if (playlistItem) {
                 vm.selectedPlaylistItem.title = playlistItem.snippet.title;
-                vm.selectedPlaylistItem.src = 'https://www.youtube.com/embed/' + playlistItem.snippet.resourceId.videoId + '?list=' + playlistItem.snippet.playlistId;
+                vm.selectedPlaylistItem.src = 'https://www.youtube.com/embed/' +
+                    playlistItem.snippet.resourceId.videoId +
+                    '?list=' + playlistItem.snippet.playlistId;
                 vm.selectedPlaylistItem.videoId = playlistItem.snippet.resourceId.videoId;
                 vm.selectedPlaylistItem.id = playlistItem.id;
-            } else {
-                vm.selectedPlaylistItem.title = 'video not found';
-                vm.selectedPlaylistItem.src = '';
-                vm.selectedPlaylistItem.videoId = '';
-                vm.selectedPlaylistItem.id = '';
+                checkIsVideoInCurrentPlaylist();
             }
-            checkIsVideoInCurrentPlaylist();
         };
 
-        vm.currentView = function(){
+        vm.currentView = function () {
             return $state.current.name;
         };
-        
-        var prevView;
 
-        vm.toggleEdit = function(){
-            if(vm.isEditMode()){
+        var prevView = null;
+
+        vm.toggleEdit = function () {
+            if (vm.isEditMode()) {
                 $state.go(!prevView || prevView == 'home.edit' ? 'home.player' : prevView);
-            }
-            else{
+            } else {
                 prevView = vm.currentView();
                 $state.go('home.edit');
             }
         };
 
-        vm.isEditMode = function(){
+        vm.isEditMode = function () {
             return vm.currentView() == 'home.edit';
         };
     }
--- app/search/searchController.js
+++ app/search/searchController.js
@@ -1,23 +1,28 @@
-(function(){
-    "use strict";
+(function () {
+    'use strict';
 
     angular
-        .module("ytApp")
-        .controller("SearchController", ["$q", "playlistService", "$scope", "searchResource", "$stateParams", SearchController]);
+        .module('ytApp')
+        .controller('SearchController', ['$q', 'playlistService', '$scope', 'searchResource', '$stateParams', SearchController]);
 
-    function SearchController($q, playlistService, $scope, searchResource, $stateParams){
+    function SearchController($q, playlistService, $scope, searchResource, $stateParams) {
         var vm = this;
         vm.playlistService = playlistService;
-        vm.searchResult =[];
+        vm.searchResult = [];
         vm.searchText = $stateParams.searchText;
 
-        var performSearch = function(){
-            searchResource.query({q: vm.searchText}, function (data) {
-                vm.searchResult = $.map(data.items, function(searchResultItem){return searchResultItem.id.kind === "youtube#channel" ? null : searchResultItem});
+        var performSearch = function () {
+            searchResource.query({
+                q: vm.searchText
+            }, function (data) {
+                vm.searchResult = $.map(data.items, function (searchResultItem) {
+                    return searchResultItem.id.kind === 'youtube#channel' ? null : searchResultItem
+                });
                 var promise = $scope.selectedPlaylist.items.length ? $q.when([]) : playlistService.fillPlaylistItems($scope.selectedPlaylist);
-                promise.then(function(playlistWithItems){
-                    if(!$scope.selectedPlaylist.items.length)
+                promise.then(function (playlistWithItems) {
+                    if (!$scope.selectedPlaylist.items.length) {
                         $scope.selectedPlaylist.items = playlistWithItems.items;
+                    }
                     for (var i = 0; i < vm.searchResult.length; i++) {
                         vm.searchResult[i].alreadyInList = isVideoInList(vm.searchResult[i]);
                     }
@@ -25,39 +30,38 @@
             });
         };
 
-        function isVideoInList(searchItem){
+        function isVideoInList(searchItem) {
             return itemIndexInPlaylist(searchItem) !== -1;
         }
 
-        vm.addToPlayList = function(searchItem){
-            var index = itemIndexInPlaylist(searchItem);
-
-            if(index === -1){
+        vm.addToPlayList = function (searchItem) {
+            if (!isVideoInList(searchItem)) {
                 playlistService.addItemToPlaylist($scope.selectedPlaylist, searchItem)
-                    .then(function(){
-                    searchItem.alreadyInList = isVideoInList(searchItem);
-                });
+                    .then(function () {
+                        searchItem.alreadyInList = isVideoInList(searchItem);
+                    });
             }
         };
 
-        var itemIndexInPlaylist = function(searchItem){
+        var itemIndexInPlaylist = function (searchItem) {
             return _.findIndex($scope.selectedPlaylist.items, function (item) {
                 return item.snippet.resourceId.videoId === searchItem.id.videoId;
             });
         };
 
-        vm.removeFromPlayList = function(searchItem){
+        vm.removeFromPlayList = function (searchItem) {
             var index = itemIndexInPlaylist(searchItem);
 
-            if(index !== -1 && searchItem.alreadyInList){
-                playlistService.removeItemFromPlaylist($scope.selectedPlaylist, $scope.selectedPlaylist.items[index])
-                    .then(function videoRemovedFromPlaylist(data){
-                    searchItem.alreadyInList = isVideoInList(searchItem);
-                });
+            if (index !== -1 && searchItem.alreadyInList) {
+                playlistService.removeItemFromPlaylist($scope.selectedPlaylist,
+                    $scope.selectedPlaylist.items[index]).then(
+                    function videoRemovedFromPlaylist(data) {
+                        searchItem.alreadyInList = isVideoInList(searchItem);
+                    });
             }
         };
 
-        playlistService.playlistsPromise.then(function(){
+        playlistService.playlistsPromise.then(function () {
             performSearch();
         });
     }
