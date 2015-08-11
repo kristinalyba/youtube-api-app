8453d263de4f9484e01d48272fdce059d5401cb5
kristina <kristina.lyba@gmail.com>
2015-08-11 17:37:32 +0300

    add PubSub factory

---
+++ src/common/factories/pubSub.js
@@ -0,0 +1,30 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('ytApp')
+        .factory('PubSub', ['$rootScope', PubSub]);
+
+    function PubSub($rootScope) {
+        function subscribe(topic, func) {
+            var cleanUpFunc = $rootScope.$on(topic, function (e, args) {
+                func(args);
+            });
+
+            //call this func to unsuscribe from event
+            //      $scope.$on('$destroy', function() {
+            //        cleanUpFunc();
+            //      });
+            return cleanUpFunc;
+        }
+
+        function publish(topic, args) {
+            $rootScope.$emit(topic, args);
+        }
+
+        return {
+            subscribe: subscribe,
+            publish: publish
+        }
+    }
+}());
